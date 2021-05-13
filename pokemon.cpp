0cd94541817a914de6ad2774d70c7d0265398c79
Kyeore Heo <91kyoheo@gmail.com>
2021-05-13 19:57:32 -0400

    Update pokemon.cpp

--- pokemon.cpp
+++ pokemon.cpp
@@ -26,35 +26,12 @@ Pokemon::~Pokemon() {
 }
 
 void Pokemon::info() {
-
-    int total = 40;
-    std::string a = "";
-    int dashes = (total - name_.size()) / 2;
-
-
-    std::cout << std::endl;
-    std::string type = "|type: " + type_;
-    std::cout << type;
-    putSpace(total - (type.size() + 1));
-    std::cout << "|";
-    std::cout << std::endl;
-
-    if(name_.size() % 2 == 0) {
-        putDash(dashes);
-        std::cout << name_;
-        putDash(dashes);
-
-    } else {
-        putDash(dashes + 1);
-        std::cout << name_;
-        putDash(dashes);
-    }
-
-    std::cout << "|friends: " << friends_.size();
-    putSpace(total - (friends.size() + 1));
-    std::cout << "|";
-    std::cout << std::endl;
-
+    displayName();
+    displayType();
+    displayHeight();
+    displayWeight();
+    displayFriends();
+    std::cout << repeatChar('-', 20) << std::endl;
 }
 
 std::string Pokemon::repeatChar(char c, int length) const {
