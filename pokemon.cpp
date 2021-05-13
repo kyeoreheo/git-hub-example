5b0905f5d52fc7978602a1b4919cce8601d172c9
Kyeore Heo <91kyoheo@gmail.com>
2021-05-13 19:48:59 -0400

    Revert "Update pokemon.cpp"
    This reverts commit 7b54fec3f6b5230ac2e2ffa03dec83b1692d8131.

--- pokemon.cpp
+++ pokemon.cpp
@@ -39,16 +39,8 @@ Pokemon::~Pokemon() {
 
 void Pokemon::info() {
 
-    int total = 40;
-    std::string = "";
+    int total = 20;
     int dashes = (total - name_.size()) / 2;
-
-
-    std::string height = "|height: " + height_;
-    std::cout << height;
-    putSpace(total - (height.size() + 1));
-    std::cout << "|";
-    std::cout << std::endl;
 
     if(name_.size() % 2 == 0) {
         putDash(dashes);
@@ -60,11 +52,39 @@ void Pokemon::info() {
         std::cout << name_;
         putDash(dashes);
     }
+    std::cout << std::endl;
+    std::string type = "|type: " + type_;
+    std::cout << type;
+    putSpace(total - (type.size() + 1));
+    std::cout << "|";
+    std::cout << std::endl;
 
+    std::string height = "|height: " + height_;
+    std::cout << height;
+    putSpace(total - (height.size() + 1));
+    std::cout << "|";
+    std::cout << std::endl;
+
+    std::string weight = "|weight: " + weight_;
+    std::cout << weight;
+    putSpace(total - (weight.size() + 1));
+    std::cout << "|";
+    std::cout << std::endl;
 
     std::cout << "|friends: " << friends_.size();
     putSpace(total - (friends.size() + 1));
     std::cout << "|";
     std::cout << std::endl;
 
+}
+
+void Pokemon::putDash(int num) {
+    for(int i = 0; i < num; i++) {
+        std::cout << "-";
+    }
+}
+void Pokemon::putSpace(int num) {
+    for(int i = 0; i < num; i++) {
+        std::cout << " ";
+    }
 }
